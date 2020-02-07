7df8d2b6ad2c33a80b7c6bdc2a1d7e9002806bef
scadler <scadler@s.sfusd.edu>
2020-02-07 23:55:00 +0000

    finished

--- Questions.js
+++ Questions.js
@@ -1,68 +1,52 @@
 let data = 
 [ { question: "How many types of wildfires are there?",
-    answer: 2,
-    options: [2,3,4,1]
-},
+    answer: "2",
+    options: ["2","3","4","1"]
+    },
 
-  { question: "How do wildfires purposely start?",
+  { question: "What purposely causes fires?",
     answer: "humans",
     options: ["humans","animals","weather","insects"]
   },
-   { question: "How do wildfires affect wildlife?",
+   { question: "At what temperature do wilfires burn?",
     answer: "1,472F",
     options: ["1,472F","1,682F","1,936F","2,125F"]
 },
-   { question: "What's the purpose of prescribed fire?",
+   { question: "What's the purpose of a prescribed fire?",
     answer: "restore ecosystem health and recycle nutrients",
-    options: ["restore ecosystem health and recycle nutrients","burn all the trees down","help animals improve their environment","create a better forest"]
+    options: ["restore ecosystem health and recycle nutrients","to burn all the trees down","help animals improve their environment","create a better forest"]
 },
-  { question: "How do wildfires affect the soils",
-    answer: "Can burn the nutrients in the soil",
-    options: ["can burn the nutrients in the soil","the soils will disappear","it can help plants grow faster","it can harm humans"]
-
+  { question: "How do wildfires affect the soil?",
+    answer: "They can burn the nutrients in the soil",
+    options: ["They can burn the nutrients in the soil","the soil will disappear","it can help plants grow faster","it can harm humans"]
   },
   { question: "Will it harm the forest if the largest dead trees are removed?",
     answer: "Yes may impact the wildlife and affect how the forest recovers",
     options: ["Yes may impact the wildlife and affect how the forest recovers","No, they're just taking up space","Yes, it can affect the air","No, they're not as useful anymore since they're dead"]
-
 },
 {  question: "How many wildfires occured in Calilforia in 2019?",
     answer: "7,860",
-    options: [7,860, 10,568,4,589,8,129]
+    options: ["7,860", "10,568","4,589","8,129"]
 },
 {
     question: "What causes wildfires naturally?",
     answer: "lightning",
-    options: ["lightning","foggy","hail","nothing"]
-
+    options: ["lightning","fog","hail","nothing"]
 },
 {
- question: "How can you survive a wildfire?",
+ question: "How can you survive an oncoming wildfire?",
     answer: "jump in a lake or river",
-    options: ["jump in a lake or river","wait until it clears out","stay burning","rub your burning body against a wall"]
-
+    options: ["jump in a lake or river","wait until it clears out","stay and burn","rub your burning body against a wall"]
 },
 {
-    question: "What percentage are caused by humans?",
+    question: "What percentage of forest fires are caused by humans?",
     answer: "85%",
-    options: ["85%","87%","90%","78%"]
-
-
-}
-
-
-
-    
-]
+    options: ["85%","87%","90%","78%"],
+},  
+];
 function randomNumbers() {
-    return Math.floor(Math.random()*(data.length-1);
+    return Math.floor(Math.random()*(data.length-1));
 }
 console.log (randomNumbers())
 data[9]
 console.log (data[randomNumbers()])
-
-
-function randomQuestions() {
-    return data[randomNumbers()]
-}
-console.log (data[randomQuestions()])
--- script.js
+++ script.js
@@ -1,17 +1,78 @@
-import Question from Question.js;
 $("#modal").hide();
+$("#modal1").hide();
+$("#modal2").hide();
+let data = getData();
 $("#statsP").hide();
 
-// $(".question").text(data[Math.floor(Math.random() * 2].question);
+var array = []
+appendQuestions();
+function appendQuestions() {
+    let index = Math.floor(Math.random()*10);
+    $("#questions").text(data[index].question);
+    for(let i=0; i<4; i++) {
+        $("#answers"+i).text(data[index].options[i]);
+    }
+     $(".ans").click(function(){
+         $("#modal").hide();
+        var thisText = $(this).text();
+    console.log(thisText)
+    if(data[index].answer === thisText){
+        var fire = array[0]
+        array.shift();
+        $(fire).remove();
+        if(array.length() === 0){
+            inprogress = false;
+        }
+
+    }
+    else{
+        var xCoor = (Math.floor((Math.random() * 4)) * 10);
+        var yCoor = (Math.floor((Math.random() * 9)));
+        var xyCoor = Number(xCoor + yCoor);
+        console.log(xyCoor)
+        if ($("#s" + xyCoor).contents().attr('class') === "fire") {
+            i = i - 1;
+        }
+        else if(xyCoor > 46){
+            i = i -1;
+        }
+        else {
+            array.push("#f"+xyCoor);
+                $("#s" + xyCoor).append(`<img class="fire" id=f${xyCoor} src="https://imgur.com/PLr4070.jpg">`);
+                var newFireCounter = Number($("#fireCounter").text()) + 1;
+            $("#fireCounter").text(newFireCounter)
+            if(newFireCounter >=11){
+                inprogress = false;
+                $("#gameStart").show();
+                $("#statsP").hide();
+            }
+    }
+}
+});
+}
+
+
+$(".quit").click(function(){
+    $("#modal").hide();
+    appendQuestions();
+})
+
+
+
+
 $(".forestTile").click(function() {
     var Id = $(this).attr('id');
+    console.log(Id)
+    var object = {
+        id: Id,
+}
     if($("#"+Id).contents().hasClass('fire') === true){
-    $("#modal").show();
-    $("#forest").css("opacity", 0.5);
+        $("#modal").show();
+        // $("#forest").css("opacity", 0.5);
     }
 });
-
-var inprogress = false
+$("#statsP").hide();
+var inprogress = false;
 $("#gameStart").click(function(){
     $("#gameStart").hide();
     $("#statsP").show();
@@ -35,11 +96,12 @@ function startFires() {
             i = i -1;
         }
         else {
+            array.push("#f"+xyCoor);
             if(fireType === 0){
-                $("#s" + xyCoor).append(`<img class="fire" id=${xyCoor} src="https://imgur.com/PLr4070.jpg">`);
+                $("#s" + xyCoor).append(`<img class="fire" id=f${xyCoor} src="https://imgur.com/PLr4070.jpg">`);
             }
             else if(fireType === 1){
-                $("#s" + xyCoor).append(`<img class="fire" id=${xyCoor} src="https://imgur.com/ZFYKZ89.jpg">`);
+                $("#s" + xyCoor).append(`<img class="fire" id=f${xyCoor} src="https://imgur.com/ZFYKZ89.jpg">`);
             }
         }
         i = i + 1;
@@ -91,11 +153,12 @@ setInterval(function(){
             b = b-1;
         }
         else {
+            array.push("#f"+xy);
             if(newFireType === 0){
-                $("#s" + xy).append(`<img class="fire" id=${xy} src="https://imgur.com/PLr4070.jpg">`);
+                $("#s" + xy).append(`<img class="fire" id=f${xy} src="https://imgur.com/PLr4070.jpg">`);
             }
             else if(newFireType === 1){
-                $("#s" + xy).append(`<img class="fire" id=${xy} src="https://imgur.com/ZFYKZ89.jpg">`);
+                $("#s" + xy).append(`<img class="fire" id=f${xy} src="https://imgur.com/ZFYKZ89.jpg">`);
             }
             var newFireCounter = Number($("#fireCounter").text()) + 1;
             $("#fireCounter").text(newFireCounter)
@@ -108,4 +171,66 @@ setInterval(function(){
         b = b+1;
     }
 }
-}, 500)
+}, 7500)
+function getData(){
+    let data = 
+[ { question: "How many types of wildfires are there?",
+    answer: 2,
+    options: ["2","3","4","1"]
+    },
+
+  { question: "What purposely causes fires?",
+    answer: "humans",
+    options: ["humans","animals","weather","insects"]
+  },
+   { question: "How do wildfires affect wildlife?",
+    answer: "1,472F",
+    options: ["1,472F","1,682F","1,936F","2,125F"]
+},
+   { question: "What's the purpose of a prescribed fire?",
+    answer: "restore ecosystem health and recycle nutrients",
+    options: ["restore ecosystem health and recycle nutrients","to burn all the trees down","help animals improve their environment","create a better forest"]
+},
+  { question: "How do wildfires affect the soil",
+    answer: "They can burn the nutrients in the soil",
+    options: ["They can burn the nutrients in the soil","the soil will disappear","it can help plants grow faster","it can harm humans"]
+  },
+  { question: "Will it harm the forest if the largest dead trees are removed?",
+    answer: "Yes may impact the wildlife and affect how the forest recovers",
+    options: ["Yes may impact the wildlife and affect how the forest recovers","No, they're just taking up space","Yes, it can affect the air","No, they're not as useful anymore since they're dead"]
+},
+{  question: "How many wildfires occured in Calilforia in 2019?",
+    answer: "7,860",
+    options: ["7,860", "10,568","4,589","8,129"]
+},
+{
+    question: "What causes wildfires naturally?",
+    answer: "lightning",
+    options: ["lightning","fog","hail","nothing"]
+},
+{
+ question: "How can you survive a wildfire?",
+    answer: "jump in a lake or river",
+    options: ["jump in a lake or river","wait until it clears out","stay and burn","rub your burning body against a wall"]
+},
+{
+    question: "What percentage of forest fires are caused by humans?",
+    answer: "85%",
+    options: ["85%","87%","90%","78%"],
+},  
+];
+    return data
+}
+// function gamewin{
+//  $("#models1").show
+// }
+
+
+// function myalert() {
+//     alert("Can't save the forest in game? Do it https://northlab.faculty.ucdavis.edu/wp-content/uploads/sites/195/2016/02/Forest-Fires-Answers-to-12-Common-Questions-2.pdf");
+// }
+
+// function myalert() {
+// alert("Congratulations! you are an expert. On more information, https://www.doi.gov/blog/7-burning-questions-wildfires-public-lands")
+ // }
+ 
